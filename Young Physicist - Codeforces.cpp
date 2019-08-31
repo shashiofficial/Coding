c3e37688ba5ef6ef004463e2f4358140c49e713b
shashiofficial <47141879+shashiofficial@users.noreply.github.com>
2019-08-31 11:45:00 +0530

    Update Young Physicist - Codeforces.cpp

--- Young Physicist - Codeforces.cpp
+++ Young Physicist - Codeforces.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	
-	int size;
+	int size;				//size of Array
 	cin>>size;
 	int a[size][3];
 	
@@ -11,12 +11,12 @@ int main(){
 		for(int j=0;j<3;j++)
 			cin>>a[i][j];	
 			
-	int c1_sum=0, c2_sum=0,c3_sum=0;
+	int c1_sum=0, c2_sum=0,c3_sum=0; 	//Store sum of columns
 	
 	for(int i=0;i<size;i++){
-		c1_sum+=a[i][0];
-		c2_sum+=a[i][1];
-		c3_sum+=a[i][2];		
+		c1_sum+=a[i][0]; 		//Adding Column 1
+		c2_sum+=a[i][1];		//Adding Column 2
+		c3_sum+=a[i][2];		//Adding Column 3
 	}
 	
 	if((c1_sum==0) && (c2_sum==0) && (c3_sum==0))
