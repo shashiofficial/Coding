3d27eb08a74aa46562a0e99ed0246cfb2d2cd004
shashiofficial <47141879+shashiofficial@users.noreply.github.com>
2019-08-31 12:34:16 +0530

    Update Beautiful Matrix-Codeforces.cpp

--- Beautiful Matrix-Codeforces.cpp
+++ Beautiful Matrix-Codeforces.cpp
@@ -7,12 +7,12 @@ int main(){
 	for(int i=0;i<5;i++)
 	 	for(int j=0;j<5;j++){
 	 		cin>>a[i][j];
-	 		if(a[i][j]==1){
-	 			i1=i;
-	 			j1=j;
+	 		if(a[i][j]==1){		//If '1' is found, store
+	 			i1=i;		//i1 stores i-th row where '1' is found
+	 			j1=j;		//j1 stores j-th row where '1' is found
 			}
 		}
-	i1=i1-2;
+	i1=i1-2;				//Subtract with 2 to get the distance
 	i1=abs(i1);	
 	j1=j1-2;
 	j1=abs(j1);	
